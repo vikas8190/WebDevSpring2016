2b6f2d08eca8070f7f81f5b95d5a4f6258286bff
vikas janardhanan <vikas@ccs.neu.edu>
2016-02-21 00:58:04 -0500

    added basic form funct

---
+++ public/assignment/services/forms.service.client.js
@@ -0,0 +1,50 @@
+/**
+ * Created by vilas on 20-02-2016.
+ */
+(function(){
+    angular
+        .module("FormBuilderApp")
+        .factory("FormService",FormService);
+    function FormService(){
+        var forms;
+        forms=[
+            {"_id": "000", "title": "Contacts", "userId": 123},
+            {"_id": "010", "title": "ToDo",     "userId": 123},
+            {"_id": "020", "title": "CDs",      "userId": 234},
+        ];
+        var service={
+            createFormForUser:createFormForUser,
+            findAllFormsForUser:findAllFormsForUser,
+            deleteFormById:deleteFormById
+            //updateFormById:updateFormById
+        };
+        return service;
+        function createFormForUser(userId, form, callback){
+            var newform;
+            newform._id=(new Date).getTime();
+            newform.userId=userId;
+            newform.title=form.title;
+            forms.push(newform);
+            callback(newform);
+        }
+        function findAllFormsForUser(userId,callback){
+            console.log("search for userid:");
+            console.log(userId);
+            var cur_forms=forms.filter(function(f){
+                return (f.userId===userId);
+            });
+            console.log("cur forms:")
+            console.log(cur_forms);
+            callback(cur_forms);
+        }
+        function deleteFormById(formId,callback){
+            for(var i=0;i<forms.length;i++){
+                if(forms[i]._id==formId){
+                    forms.splice(i,1);
+                    break;
+                }
+            }
+            callback(forms);
+        }
+    }
+})();
--- public/assignment/views/forms/forms.controller.js
+++ public/assignment/views/forms/forms.controller.js
@@ -1,3 +1,27 @@
 /**
  * Created by vilas on 17-02-2016.
  */
+(function(){
+    angular
+        .module("FormBuilderApp")
+        .controller("FormController",FormController);
+    function FormController($scope,$rootScope,FormService){
+        console.log($rootScope.user._id);
+        findForms();
+        $scope.renderform=renderform;
+        $scope.deleteForm=deleteForm;
+        function findForms(){
+            FormService.findAllFormsForUser($rootScope.user._id,renderform);
+        }
+        function deleteForm(formid){
+            FormService.deleteFormById(formid,findForms);
+        }
+        function form_after_del(forms){
+            $scope.forms=forms;
+        }
+        function renderform(forms){
+            console.log(forms);
+            $scope.forms=forms;
+        }
+    }
+})();
