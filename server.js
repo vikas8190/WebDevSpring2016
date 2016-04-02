81237a9c076e721d6385ce8a75bd3ac0674f7234
vikas janardhanan <vikas@ccs.neu.edu>
2016-04-01 20:34:07 -0400

    mongodb conn string modified

--- server.js
+++ server.js
@@ -5,12 +5,15 @@ var multer=require('multer');
 
 var mongoose=require("mongoose");
 var mongo_conn_string='mongodb://localhost/form-maker';
-if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+/*if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
     mongo_conn_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
         process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
         process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
         process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
         process.env.OPENSHIFT_APP_NAME;
+}*/
+if(process.env.OPENSHIFT_MONGODB_DB_HOST) {
+    mongo_conn_string = 'mongodb://' + process.env.OPENSHIFT_MONGODB_DB_HOST + ':' + process.env.OPENSHIFT_MONGODB_DB_PORT + '/';
 }
 var db=mongoose.connect(mongo_conn_string);
 var passport=require("passport");
@@ -22,7 +25,11 @@ app.use(multer());
 app.use(cookieParser());
 app.use(session({secret:'meanstackisthebest'}));
 app.use(passport.initialize());
-app.use(passport.session());
+app.use(passport.session(
+    {resave: true,
+    saveUninitialized: true,
+    secret: process.env.PASSPORT_SECRET}
+));
 app.use(express.static(__dirname + '/public'));
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
